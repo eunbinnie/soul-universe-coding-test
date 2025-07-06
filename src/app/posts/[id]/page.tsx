0baf10593ce69540675d60795488bd4f4b88b182
eunbinnie <dmsqlsdl54@gmail.com>
2025-07-06 20:54:33 +0900

    feat: 게시글 상세 조회를 위한 ID 기반 데이터 불러오기 함수 구현

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,4 +1,14 @@
+'use client';
+
+import { usePostStore } from '@/store';
+import { useParams } from 'next/navigation';
+
 const PostDetailPage = () => {
+  const params = useParams<{ id: string }>();
+
+  const detailPost = usePostStore((state) => state.detailPost(params.id));
+  console.log(detailPost);
+
   return <div>게시글 상세</div>;
 };
 
--- src/store/actions/postStore.ts
+++ src/store/actions/postStore.ts
@@ -6,15 +6,22 @@ import type { Post } from '@/types/post.types';
 interface PostStore {
   posts: Post[];
   updatePost: (newPost: Post) => void; // 게시글 추가 함수
+  detailPost: (uuid: string) => Post | undefined; // 게시글 상세 조회 함수
 }
 
 // 게시글 목록을 로컬 스토리지에 저장하는 스토어
 export const usePostStore = create<PostStore>()(
   persist(
-    (set) => ({
+    (set, get) => ({
       posts: [],
       updatePost: (newPost: Post) =>
         set((state) => ({ posts: [newPost, ...state.posts] })),
+      detailPost: (uuid: string) => {
+        const post = get().posts.find((post) => post.uuid === uuid);
+        if (post) {
+          return post;
+        }
+      },
     }),
     { name: 'post-storage' },
   ),
