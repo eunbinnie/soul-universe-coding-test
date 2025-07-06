63e1c407251a6127baf12ab0276dc3ea7c15a538
eunbinnie <dmsqlsdl54@gmail.com>
2025-07-06 21:24:04 +0900

    refactor: 중복된 로딩 처리 로직 ClientOnly 컴포넌트로 분리

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,15 +1,28 @@
 'use client';
 
 import { usePostStore } from '@/store';
+import { Frown } from 'lucide-react';
 import { useParams } from 'next/navigation';
+
+import ClientOnly from '@/components/ClientOnly';
 
 const PostDetailPage = () => {
   const params = useParams<{ id: string }>();
 
   const detailPost = usePostStore((state) => state.detailPost(params.id));
-  console.log(detailPost);
 
-  return <div>게시글 상세</div>;
+  return (
+    <ClientOnly>
+      {detailPost ? (
+        <div>게시글 상세</div>
+      ) : (
+        <div className='mt-20 flex flex-col items-center gap-4'>
+          <Frown />
+          <p className='text-center text-sm'>게시글을 찾을 수 없습니다.</p>
+        </div>
+      )}
+    </ClientOnly>
+  );
 };
 
 export default PostDetailPage;
--- src/app/posts/components/PostListSection.tsx
+++ src/app/posts/components/PostListSection.tsx
@@ -1,44 +1,32 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
-
 import { usePostStore } from '@/store';
-import { LoaderCircle, SmilePlus } from 'lucide-react';
+import { SmilePlus } from 'lucide-react';
+
+import ClientOnly from '@/components/ClientOnly';
 
 import PostList from './PostList';
 
 const PostListSection = () => {
   const posts = usePostStore((state) => state.posts);
 
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    setIsLoading(false);
-  }, []);
-
-  if (isLoading) {
-    return (
-      <section className='mt-20 w-full'>
-        <LoaderCircle className='mx-auto animate-spin' />
-      </section>
-    );
-  }
-
   return (
-    <section>
-      {posts.length > 0 ? (
-        <PostList />
-      ) : (
-        // 등록된 게시글이 없을 경우
-        <div className='mt-20 flex flex-col items-center gap-4'>
-          <SmilePlus />
-          <p className='text-center text-sm'>
-            아직 등록된 게시글이 없습니다.
-            <br />첫 글을 작성해보세요!
-          </p>
-        </div>
-      )}
-    </section>
+    <ClientOnly>
+      <section>
+        {posts.length > 0 ? (
+          <PostList />
+        ) : (
+          // 등록된 게시글이 없을 경우
+          <div className='mt-20 flex flex-col items-center gap-4'>
+            <SmilePlus />
+            <p className='text-center text-sm'>
+              아직 등록된 게시글이 없습니다.
+              <br />첫 글을 작성해보세요!
+            </p>
+          </div>
+        )}
+      </section>
+    </ClientOnly>
   );
 };
 
