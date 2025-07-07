049b5770f0f55412e759fb1d5b788b1299ae383a
eunbinnie <dmsqlsdl54@gmail.com>
2025-07-07 22:32:09 +0900

    refactor: 댓글 작성 시간 표시

--- src/app/posts/[id]/_components/CommentList.tsx
+++ src/app/posts/[id]/_components/CommentList.tsx
@@ -13,7 +13,7 @@ const CommentList = ({ comments }: CommentListProps) => {
         <div key={comment.uuid} className='mb-3 rounded-2xl bg-zinc-100 p-5'>
           <div>
             <span className='text-xs font-bold text-zinc-900'>
-              {dayjs(comment.createdAt).format('YYYY-MM-DD')}
+              {dayjs(comment.createdAt).format('YYYY.MM.DD HH:mm')}
             </span>
           </div>
           <span className='whitespace-pre text-sm font-medium text-zinc-700'>
